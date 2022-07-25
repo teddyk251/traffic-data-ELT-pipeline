7cbe1fba6b4732b5d7e04a87688dc3f873008523
Teddy <teddylnk1@gmail.com>
2022-07-25 08:41:10 +0300

    update dbname

--- scripts/db_connector.py
+++ scripts/db_connector.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sqlalchemy import text
 
 from sqlalchemy import create_engine
-engine = create_engine('postgresql+psycopg2://teddy:123456@localhost/demo')
+engine = create_engine('postgresql+psycopg2://teddy:123456@localhost/traffic_data')
 
 VEHICLE_SCHEMA = "vehicle_data_schema.sql"
 TRAJECTORIES_SCHEMA = "trajectories_data_schema.sql"
@@ -12,7 +12,7 @@ def create_table():
     try:
         with engine.connect() as conn:
             for name in [VEHICLE_SCHEMA, TRAJECTORIES_SCHEMA]:
-                with open(f'./schema/{name}') as file:
+                with open(f'../schema/{name}') as file:
                     query = text(file.read())
                     conn.execute(query)
         print("Successfull")
@@ -20,7 +20,7 @@ def create_table():
         print(e)
 
 
-create_table()
+# create_table()
 
 def insert_table(df: pd.DataFrame, name: str ):
     try:
@@ -28,4 +28,6 @@ def insert_table(df: pd.DataFrame, name: str ):
             df.to_sql(name=name, con=conn, if_exists='replace', index=False)
     except Exception as e:
         print(f"Failed  ---- {e}")  
+
+
 
